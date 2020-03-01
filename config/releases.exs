a9a578a03f655437a2a42ab53779e7a92951a8a3
Mitchell Hanberg <mitch@mitchellhanberg.com>
2020-02-29 23:27:26 -0500

    Add GH Action

--- config/releases.exs
+++ config/releases.exs
@@ -2,5 +2,6 @@ import Config
 
 config :pipsqueak, PipsqueakWeb.Endpoint,
   server: true,
-  http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
+  # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
+  http: [port: {:system, "PORT"}],
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
--- lib/pipsqueak_web/views/page_view.ex
+++ lib/pipsqueak_web/views/page_view.ex
@@ -1,3 +1,7 @@
 defmodule PipsqueakWeb.PageView do
   use PipsqueakWeb, :view
+
+  def source_version do
+    System.get_env("SOURCE_VERSION")
+  end
 end
