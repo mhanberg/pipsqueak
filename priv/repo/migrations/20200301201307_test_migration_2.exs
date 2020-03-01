c27ea52cba5cff2112ce0a125fbcda4ef1e03aa2
Mitchell Hanberg <mitch@mitchellhanberg.com>
2020-03-01 17:19:45 -0500

    Make both migrations work

--- priv/repo/migrations/20200301201307_test_migration_2.exs
+++ priv/repo/migrations/20200301201307_test_migration_2.exs
@@ -3,9 +3,5 @@ defmodule Pipsqueak.Repo.Migrations.TestMigration2 do
 
   def change do
     IO.puts("migration 2")
-
-    unless Enum.member?([:dev, :test], Mix.env()) do
-      raise "BOOM 2"
-    end
   end
 end
